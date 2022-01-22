65af80a2bb293ce5382a90b167fe6588f170b1e8
EMLiO <emil@kollek.net>
2022-01-22 11:35:38 +0200

    Login fragment

--- app/src/main/java/com/example/appproject/activity_base.java
+++ app/src/main/java/com/example/appproject/activity_base.java
@@ -10,6 +10,8 @@ public class activity_base extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+
     }
 
 
