4bbbc32e0d0c99380d8ac8cfb6852ab5117cc547
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-02-28 17:46:04 +0200

    Added actions to navgraph to sign in and register

--- app/src/main/java/com/example/appproject/FragmentWelcome.java
+++ app/src/main/java/com/example/appproject/FragmentWelcome.java
@@ -3,10 +3,12 @@ package com.example.appproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,17 @@ public class FragmentWelcome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        Button signInBtn = view.findViewById(R.id.welcome_sign_in_btn);
+        Button registerBtn = view.findViewById(R.id.welcome_register_btn);
+        signInBtn.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.action_fragmentWelcome_to_fragmentSignIn);
+        });
+        registerBtn.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.action_fragmentWelcome_to_fragmentRegister);
+        });
+
+        return view;
     }
 }
