b883846a12fa9cd3cea2d10565c3c6e5281a3eb5
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-03-03 18:43:42 +0200

    Added picasso and MDC dependencies and ensured firebase doesn't use local cache

--- app/src/main/java/com/example/appproject/model/ModelFirebase.java
+++ app/src/main/java/com/example/appproject/model/ModelFirebase.java
@@ -1,7 +1,16 @@
 package com.example.appproject.model;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 public class ModelFirebase {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public ModelFirebase() {
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build();
+        db.setFirestoreSettings(settings);
+    }
+
 }
