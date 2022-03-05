03201bf9e403395c3cc8d092160191c4ce7f83b5
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-03-05 15:09:53 +0200

    Update to Register through Firebase

--- app/src/main/java/com/example/appproject/model/UserListener.java
+++ app/src/main/java/com/example/appproject/model/UserListener.java
@@ -3,5 +3,5 @@ package com.example.appproject.model;
 import com.google.firebase.auth.FirebaseUser;
 
 public interface UserListener {
-    void onComplete(FirebaseUser user);
+    void onComplete(FirebaseUser user, String message);
 }
