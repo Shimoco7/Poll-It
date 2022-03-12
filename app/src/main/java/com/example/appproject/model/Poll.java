df79568486f2dfe3208d5cc10d8e274035c0ab53
Amir Titelman <amirti7@gmail.com>
2022-03-12 16:28:42 +0200

    added poll logic , changes in poll question and image

---
+++ app/src/main/java/com/example/appproject/model/Poll.java
@@ -0,0 +1,19 @@
+package com.example.appproject.model;
+
+public  class Poll {
+
+    public String question;
+    public String answer1;
+    public String answer2;
+    public String answer3;
+
+
+
+    public Poll(String question, String answer1, String answer2, String answer3) {
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+    }
+}
+
