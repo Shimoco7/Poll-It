126ad48786e48add5516926604f69029f98f4009
adargvili <adar555@gmail.com>
2022-03-14 16:24:24 +0200

    minor

--- app/src/main/java/com/example/appproject/details/DetailsHolder.java
+++ app/src/main/java/com/example/appproject/details/DetailsHolder.java
@@ -8,7 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appproject.R;
-import com.example.appproject.model.detail.Detail;
 import com.example.appproject.model.question.Question;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -16,12 +15,12 @@ public class DetailsHolder extends RecyclerView.ViewHolder {
 
     TextInputLayout questionTv;
 
-    AutoCompleteTextView answersAc;
+    AutoCompleteTextView multiChoiceAc;
 
     public DetailsHolder(@NonNull View itemView) {
         super(itemView);
         questionTv = itemView.findViewById(R.id.details_list_tv);
-        answersAc = itemView.findViewById(R.id.details_list_ac);
+        multiChoiceAc = itemView.findViewById(R.id.details_list_ac);
 
     }
 
@@ -30,8 +29,8 @@ public class DetailsHolder extends RecyclerView.ViewHolder {
         questionTv.setHint(question.getQuestion());
         questionTv.setTag(question.getQuestionId());
         String[] array = question.getMultiChoice().toArray(new String[0]);
-        ArrayAdapter adapter = new ArrayAdapter<>(answersAc.getContext(), R.layout.drop_down, array);
-        answersAc.setAdapter(adapter);
+        ArrayAdapter adapter = new ArrayAdapter<>(multiChoiceAc.getContext(), R.layout.drop_down, array);
+        multiChoiceAc.setAdapter(adapter);
 //        answersAc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
