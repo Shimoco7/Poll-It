e95f5c201b6e85c8075f3489a1bc3ac291e02816
adargvili <adar555@gmail.com>
2022-03-19 10:06:06 +0200

    minor reformat

--- app/src/main/java/com/example/appproject/details/FragmentUserImage.java
+++ app/src/main/java/com/example/appproject/details/FragmentUserImage.java
@@ -2,12 +2,10 @@ package com.example.appproject.details;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -17,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.example.appproject.MainActivity;
 import com.example.appproject.MyApplication;
@@ -25,23 +22,18 @@ import com.example.appproject.R;
 import com.example.appproject.model.General;
 import com.example.appproject.model.Model;
 import com.example.appproject.model.detail.Detail;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
-
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class FragmentUserImage extends Fragment {
 
-    Button finishBtn,backBtn;
-    MaterialButton galleryBtn,camBtn;
+    Button finishBtn, backBtn;
+    MaterialButton galleryBtn, camBtn;
     ProgressBar progressBar;
     ActivityResultLauncher<Void> cameraActivityResultLauncher;
     ActivityResultLauncher<String> galleryActivityResultLauncher;
@@ -70,7 +62,7 @@ public class FragmentUserImage extends Fragment {
 
 
     private void setUserAvatar() {
-        if(MyApplication.getUserProfilePicUrl().equals("")) {
+        if (MyApplication.getUserProfilePicUrl().equals("")) {
             Model.instance.getUserDetailById(MyApplication.getUserKey(), "Gender", gender -> {
                 if (gender.getAnswer().equals("Female")) {
                     userAvatar.setImageResource(R.drawable.female_avatar);
@@ -78,14 +70,14 @@ public class FragmentUserImage extends Fragment {
                     userAvatar.setImageResource(R.drawable.avatar);
                 }
             });
-        }
-        else {
+        } else {
             progressBar.setVisibility(View.VISIBLE);
             Picasso.get().load(MyApplication.getUserProfilePicUrl()).into(userAvatar, new Callback() {
                 @Override
                 public void onSuccess() {
                     progressBar.setVisibility(View.GONE);
                 }
+
                 @Override
                 public void onError(Exception e) {
                     progressBar.setVisibility(View.GONE);
@@ -102,7 +94,7 @@ public class FragmentUserImage extends Fragment {
         cameraActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.TakePicturePreview(),
                 result -> {
-                    if(result!=null){
+                    if (result != null) {
                         bitMap = result;
                         userAvatar.setImageBitmap(bitMap);
                     }
@@ -113,7 +105,7 @@ public class FragmentUserImage extends Fragment {
                 new ActivityResultContracts.GetContent(),
                 result -> {
                     try {
-                        bitMap = MediaStore.Images.Media.getBitmap(this.getContext().getContentResolver(),result);
+                        bitMap = MediaStore.Images.Media.getBitmap(this.getContext().getContentResolver(), result);
                     } catch (IOException ignored) {
                     }
                     userAvatar.setImageBitmap(bitMap);
@@ -124,35 +116,35 @@ public class FragmentUserImage extends Fragment {
             openGallery();
         });
 
-        finishBtn.setOnClickListener(v->{
+        finishBtn.setOnClickListener(v -> {
             finish(container);
         });
 
-        backBtn.setOnClickListener(v->{
+        backBtn.setOnClickListener(v -> {
             Navigation.findNavController(v).navigateUp();
         });
     }
 
     private void finish(ViewGroup container) {
-        General.progressBarOn(getActivity(),container,progressBar);
-        Model.instance.getAllDetails(MyApplication.getUserKey(),list->{
-            for(Detail d : list){
-                if(d.getQuestion().equals("Gender")){
-                    Model.instance.updateUser(d.getUserUid(),"gender",d.getAnswer(),()->{});
+        General.progressBarOn(getActivity(), container, progressBar);
+        Model.instance.getAllDetails(MyApplication.getUserKey(), list -> {
+            for (Detail d : list) {
+                if (d.getQuestion().equals("Gender")) {
+                    Model.instance.updateUser(d.getUserUid(), "gender", d.getAnswer(), () -> {
+                    });
                 }
-                Model.instance.saveDetailOnRemoteDb(d,()->{});
+                Model.instance.saveDetailOnRemoteDb(d, () -> {
+                });
             }
-            if(bitMap == null){
+            if (bitMap == null) {
                 toMainActivity();
-            }
-            else{
-                Model.instance.saveImage(bitMap,MyApplication.getUserKey()+".jpg",url->{
-                    if(url == null){
-                        General.showToast(getActivity(),new ArrayList<>(Collections.singletonList(getString(R.string.image_upload_failed))));
-                        General.progressBarOff(getActivity(),container,progressBar);
-                    }
-                    else{
-                        Model.instance.updateUser(MyApplication.getUserKey(),"profile_pic_url",url, this::toMainActivity);
+            } else {
+                Model.instance.saveImage(bitMap, MyApplication.getUserKey() + ".jpg", url -> {
+                    if (url == null) {
+                        General.showToast(getActivity(), new ArrayList<>(Collections.singletonList(getString(R.string.image_upload_failed))));
+                        General.progressBarOff(getActivity(), container, progressBar);
+                    } else {
+                        Model.instance.updateUser(MyApplication.getUserKey(), "profile_pic_url", url, this::toMainActivity);
                         MyApplication.setUserProfilePicUrl(url);
                     }
                 });
@@ -168,7 +160,7 @@ public class FragmentUserImage extends Fragment {
         galleryActivityResultLauncher.launch("image/*");
     }
 
-    private void toMainActivity(){
+    private void toMainActivity() {
         Intent intent = new Intent(getContext(), MainActivity.class);
         startActivity(intent);
         getActivity().finish();
