5923dec080ce6fff7a87719f62175dffacdc2cb0
EMLiO <emil@kollek.net>
2022-04-20 14:21:45 +0300

    Arranging folders

--- app/src/main/java/com/example/appproject/poll/FragmentQuestionImage.java
+++ app/src/main/java/com/example/appproject/poll/FragmentQuestionImage.java
@@ -1,4 +1,4 @@
-package com.example.appproject;
+package com.example.appproject.poll;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.appproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,6 +56,7 @@ public class FragmentQuestionImage extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
