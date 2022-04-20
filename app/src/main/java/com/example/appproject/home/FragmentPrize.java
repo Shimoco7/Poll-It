401a54c203090e6a9bdd85559bc7f4ff01a8a9d1
EMLiO <emil@kollek.net>
2022-04-20 14:13:03 +0300

    Prize fragment

--- app/src/main/java/com/example/appproject/home/FragmentPrize.java
+++ app/src/main/java/com/example/appproject/home/FragmentPrize.java
@@ -1,19 +1,26 @@
-package com.example.appproject;
+package com.example.appproject.home;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.appproject.MyApplication;
+import com.example.appproject.R;
+import com.example.appproject.model.Model;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentRewardCenter#newInstance} factory method to
+ * Use the {@link FragmentPrize#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentRewardCenter extends Fragment {
+public class FragmentPrize extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +31,7 @@ public class FragmentRewardCenter extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentRewardCenter() {
+    public FragmentPrize() {
         // Required empty public constructor
     }
 
@@ -34,11 +41,11 @@ public class FragmentRewardCenter extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment rewardCenter.
+     * @return A new instance of fragment prize.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentRewardCenter newInstance(String param1, String param2) {
-        FragmentRewardCenter fragment = new FragmentRewardCenter();
+    public static FragmentPrize newInstance(String param1, String param2) {
+        FragmentPrize fragment = new FragmentPrize();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,7 +65,22 @@ public class FragmentRewardCenter extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reward_center, container, false);
+        View view = inflater.inflate(R.layout.fragment_prize, container, false);
+        Button collectBtn=view.findViewById(R.id.prize_btn_collect);
+        collectBtn.setOnClickListener((v -> {
+            popup();
+        }));
+        return view;
+    }
+
+    private void popup(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setMessage("Are You Sure?")
+                .setPositiveButton("BUY", (dialog, which) -> {
+                    //Navigation.findNavController(v).navigate(FragmentHomeScreenDirections.actionFragmentHomeScreenToFragmentActivePoll(pollId));
+                })
+                .setNeutralButton("Cancel", null);
+        AlertDialog alert1 = alert.create();
+        alert1.show();
     }
 }
