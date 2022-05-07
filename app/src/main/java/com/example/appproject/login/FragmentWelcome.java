f28d1571a114afdf8bb099af67964a6e5c89ff3a
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-05-07 12:57:13 +0300

    got email, id and name upon login from facebook

--- app/src/main/java/com/example/appproject/login/FragmentWelcome.java
+++ app/src/main/java/com/example/appproject/login/FragmentWelcome.java
@@ -2,6 +2,7 @@ package com.example.appproject.login;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -15,10 +16,17 @@ import com.example.appproject.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
+import java.util.Collections;
 
 
 public class FragmentWelcome extends Fragment {
@@ -34,25 +42,30 @@ public class FragmentWelcome extends Fragment {
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
         CallbackManager callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setPermissions(Arrays.asList(EMAIL));
+        loginButton.setPermissions(Collections.singletonList(EMAIL));
         loginButton.setFragment(this);
-        // If you are using in a fragment, call loginButton.setFragment(this);
 
-        // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d("TAG", loginResult.getAccessToken().getToken());
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), (jsonObject, graphResponse) -> {
+                    //TODO - get AccessToken, Id, Name and Email
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
             public void onCancel() {
-                // App code
+                //TODO - handle on cancel to Login via Facebook
             }
 
             @Override
             public void onError(FacebookException exception) {
-                Log.d("TAG", exception.getLocalizedMessage());
+                //TODO - handle on Error to Login via Facebook
             }
         });
         Button signInBtn = view.findViewById(R.id.welcome_sign_in_btn);
