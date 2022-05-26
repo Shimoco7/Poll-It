b6696895e40214f02e1714f6037577fa7a1d92e9
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-05-26 12:57:49 +0300

    https update

--- app/src/main/java/com/example/appproject/model/ModelNode.java
+++ app/src/main/java/com/example/appproject/model/ModelNode.java
@@ -40,6 +40,7 @@ import javax.net.ssl.X509TrustManager;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -56,11 +57,10 @@ public class ModelNode {
     private final String BASE_URL_SERVER = "https://poll-it.cs.colman.ac.il";
 
     public ModelNode() {
-
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL_SERVER)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(getUnsafeOkHttpClient())
+                .client(General.getUnsafeOkHttpClient())
                 .build();
         methodsInterface = retrofit.create(ModelMethodsInterface.class);
     }
@@ -538,43 +538,6 @@ public class ModelNode {
                 listener.onComplete(false);
             }
         });
-    }
-
-    public static OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            final TrustManager[] trustAllCerts = new TrustManager[] {
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
-
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
-            builder.hostnameVerifier((hostname, session) -> true);
-
-            return builder.build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
 
